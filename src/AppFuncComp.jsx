af81ec2e0e9dbf1359e01e8df9aa9ba2d05e7cba
TheBigH50 <HFangmeier@outlook.com>
2022-09-12 12:14:30 -0500

    refactor App as AppFuncComp

---
+++ src/AppFuncComp.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import FilmsListFuncComp from "filmListFuncComp";
+
+function AppFuncComp(props) {
+
+let [list, setList] = useState(["ready", "set", "GO"]);
+let [text, setText] = useState("");
+
+function onSubmit(event) {
+    event.preventDefault();
+
+let newList = [...list, text];
+setList({newList, text});
+}
+
+    return (
+        <div>
+          <h1>Hello World</h1>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              value={text}
+              onChange={(e) => setText({ text: e.target.value })}
+            />
+            <button type="submit">Add</button>
+          </form>
+          <ul>
+            {list.map((text, idx) => (
+              <li key={idx}>{text}</li>
+            ))}
+          </ul>
+          <FilmsListFuncComp />
+        </div>
+      );
+}
+
+export default AppFuncComp;
