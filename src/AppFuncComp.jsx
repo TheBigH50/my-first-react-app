b3e62ad534fb1a4a7b964c33f267440941eae4f0
TheBigH50 <HFangmeier@outlook.com>
2022-09-12 13:36:12 -0500

    add setText(); to clear input on submit

--- src/AppFuncComp.jsx
+++ src/AppFuncComp.jsx
@@ -2,36 +2,36 @@ import { useState } from "react";
 import FilmsListFuncComp from "./filmListFuncComp";
 
 function AppFuncComp(props) {
+  let [list, setList] = useState(["ready", "set", "GO"]);
+  let [text, setText] = useState("");
 
-let [list, setList] = useState(["ready", "set", "GO"]);
-let [text, setText] = useState("");
-
-function onSubmit(event) {
+  function onSubmit(event) {
     event.preventDefault();
 
-let newList = [...list, text];
-setList({newList, text});
-}
+    let newList = [...list, text];
+    setList(newList);
+    setText("");
+  }
 
-    return (
-        <div>
-          <h1>Hello World</h1>
-          <form onSubmit={onSubmit}>
-            <input
-              type="text"
-              value={text}
-              onChange={(e) => setText({ text: e.target.value })}
-            />
-            <button type="submit">Add</button>
-          </form>
-          <ul>
-            {list.map((text, idx) => (
-              <li key={idx}>{text}</li>
-            ))}
-          </ul>
-          <FilmsListFuncComp />
-        </div>
-      );
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {list.map((text, idx) => (
+          <li key={idx}>{text}</li>
+        ))}
+      </ul>
+      <FilmsListFuncComp />
+    </div>
+  );
 }
 
 export default AppFuncComp;
