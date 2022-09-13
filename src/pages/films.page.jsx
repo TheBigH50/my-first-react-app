dd932287e2c402b4426d2adb3e299ce207d0ea7c
TheBigH50 <HFangmeier@outlook.com>
2022-09-13 14:51:49 -0500

    fix label in selector form

--- src/pages/films.page.jsx
+++ src/pages/films.page.jsx
@@ -1,42 +1,43 @@
 import { useState, useEffect } from "react";
 
 function FilmsPage(props) {
+  let [list, setList] = useState([]);
+  let [searchDirector, setSearchDirector] = useState("");
 
-    let [list, setList] = useState([])
-    let [searchDirector, setSearchDirector] = useState("")
-
-function getFilms() {
+  function getFilms() {
     fetch("https://ghibliapi.herokuapp.com/films")
-    .then((res) => res.json())
-    .then((films) => setList(films))
-    .catch((error) => console.error(error));    
-}
+      .then((res) => res.json())
+      .then((films) => setList(films))
+      .catch((error) => console.error(error));
+  }
 
-useEffect(() => {
-getFilms();
-}, []);
+  useEffect(() => {
+    getFilms();
+  }, []);
 
-    return (
-        <div>
-            <h1>Studio Ghibli Films</h1>
-            <form>
-                <div className= "form-group">
-                    <label>
-                        <select
-                        value = {searchDirector}
-                        onChange={(e) => setSearchDirector(e.option.value)}
-                        >
-                        <option value="">All</option>
-                            </select></label>
-                </div>
-            </form>
-        <ul>
-       {list.map((film) => (
-              <li key={film.id}>{film.title}</li>
-            ))}
-        </ul>
+  return (
+    <div>
+      <h1>Studio Ghibli Films</h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor="searchDirector">Filter By Director</label>
+          <select
+            name="searchDirector"
+            id="searchDirector"
+            value={searchDirector}
+            onChange={(e) => setSearchDirector(e.target.value)}
+          >
+            <option value="">All</option>
+          </select>
         </div>
-      );
+      </form>
+      <ul>
+        {list.map((film) => (
+          <li key={film.id}>{film.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default FilmsPage;
