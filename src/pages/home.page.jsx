4da17761973af3cf04f103ddc578d6e5060fefbe
TheBigH50 <HFangmeier@outlook.com>
2022-09-13 13:25:57 -0500

    Exe1

---
+++ src/pages/home.page.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+
+function HomePage(props) {
+  let [list, setList] = useState(["ready", "set", "GO"]);
+  let [text, setText] = useState("");
+
+  function onSubmit(event) {
+    event.preventDefault();
+
+    let newList = [...list, text];
+    setList(newList);
+    setText("");
+  }
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="listitem"
+          id="listitem"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {list.map((text, idx) => (
+          <li key={idx}>{text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default AppFuncComp;
