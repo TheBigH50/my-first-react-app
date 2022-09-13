8c3788c0e7ab4420d6d2428b9a771c0c93691b0d
TheBigH50 <HFangmeier@outlook.com>
2022-09-13 14:20:48 -0500

    Step2 Exe1

--- src/pages/films.page.jsx
+++ src/pages/films.page.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 function FilmsPage(props) {
 
     let [list, setList] = useState([])
+    let [searchDirector, setSearchDirector] = useState("")
 
 function getFilms() {
     fetch("https://ghibliapi.herokuapp.com/films")
@@ -18,6 +19,17 @@ getFilms();
     return (
         <div>
             <h1>Studio Ghibli Films</h1>
+            <form>
+                <div className= "form-group">
+                    <label>
+                        <select
+                        value = {searchDirector}
+                        onChange={(e) => setSearchDirector(e.option.value)}
+                        >
+                        <option value="">All</option>
+                            </select></label>
+                </div>
+            </form>
         <ul>
        {list.map((film) => (
               <li key={film.id}>{film.title}</li>
