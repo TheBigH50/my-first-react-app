3096bce3d4b34a1dbc90a43bc08bab17795d6e44
TheBigH50 <HFangmeier@outlook.com>
2022-09-15 12:04:10 -0500

    finish mainlayout.component, prettier

--- src/components/layout/topnav/topnav.component.jsx
+++ src/components/layout/topnav/topnav.component.jsx
@@ -5,32 +5,32 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
-function TopNav() {
-    <Navbar bg="dark" variant="dark" expand="lg">
-  <Container>
-    <Navbar.Brand href="/">
-      <img
-        alt=""
-        src={reactlogo}
-        height="40"
-        className="d-inline-block align-top"
-      />{" "}
-      Learning React
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <NavLink className="nav-link" to="/">
-          Home
-        </NavLink>
-        <NavLink className="nav-link" to="/films">
-          Films
-        </NavLink>
-      </Nav>
-      <Navbar.Brand className="justify-content-end">
-        <img src={tclogo} height="40" alt="TrueCoders" />
+export function TopNav() {
+  <Navbar bg="dark" variant="dark" expand="lg">
+    <Container>
+      <Navbar.Brand href="/">
+        <img
+          alt=""
+          src={reactlogo}
+          height="40"
+          className="d-inline-block align-top"
+        />{" "}
+        Learning React
       </Navbar.Brand>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <NavLink className="nav-link" to="/">
+            Home
+          </NavLink>
+          <NavLink className="nav-link" to="/films">
+            Films
+          </NavLink>
+        </Nav>
+        <Navbar.Brand className="justify-content-end">
+          <img src={tclogo} height="40" alt="TrueCoders" />
+        </Navbar.Brand>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>;
 }
